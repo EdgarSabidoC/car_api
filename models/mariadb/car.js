64aac5b020e023cd14595bf2956d1ba2bed4ac61
Edgar Sabido C <edgar.sabido17@gmail.com>
2023-05-08 01:25:33 -0600

    Se creó la función getItems para carModel

--- models/mariadb/car.js
+++ models/mariadb/car.js
@@ -3,35 +3,56 @@ const { DataTypes } = require("sequelize");
 
 // The first part is the structure of the schema:
 const Car = sequelize.define(
-	"tracks",
+	"car",
 	{
-		name: {
+		vin: {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
-		album: {
-			type: DataTypes.STRING,
-		},
-		cover: {
-			type: DataTypes.STRING,
-		},
-		artist_name: {
-			type: DataTypes.STRING,
-		},
-		artist_nickname: {
-			type: DataTypes.STRING,
-		},
-		artist_nationality: {
-			type: DataTypes.STRING,
-		},
-		duration_start: {
+		mileage: {
 			type: DataTypes.NUMBER,
+			allowNull: false,
 		},
-		duration_end: {
-			type: DataTypes.NUMBER,
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: false,
 		},
-		mediaId: {
+		purchase_price: {
 			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		sale_price: {
+			type: DataTypes.NUMBER,
+			allowNull: true,
+		},
+		maintenance_cost: {
+			type: DataTypes.NUMBER,
+			defaultValue: 0,
+			allowNull: false,
+		},
+		model: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		car_condition: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		interior_color: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		exterior_color: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		dealership: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		sold: {
+			type: DataTypes.BOOLEAN,
+			defaultValue: 0,
 		},
 	},
 	{
